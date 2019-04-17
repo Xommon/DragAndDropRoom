2f93f09a660a733c1cec5c973e8de7ef147d2f0c
Micheal Quentin <mquenti3@gmail.com>
2019-04-17 00:01:05 -0500

    Able to select and deselect object

---
+++ DragAndDropRoom/Assets/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject selected;
+    Ray ray;
+    RaycastHit hit;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit) && Input.GetMouseButtonDown(0))
+        {
+            if (hit.collider.gameObject.tag == "MoveableObject")
+            {
+                selected = hit.collider.gameObject;
+                Debug.Log(selected.name);
+            }
+            else
+            {
+                selected = null;
+                Debug.Log("no object");
+            }
+        }
+    }
+}
