f8ef50825df42ee42d9dd078dc286b0abb29cbcd
Micheal Quentin <mquenti3@gmail.com>
2019-04-22 19:02:07 -0500

    Recoded camera movements

--- DragAndDropRoom/Assets/GameManager.cs
+++ DragAndDropRoom/Assets/GameManager.cs
@@ -8,9 +8,18 @@ public class GameManager : MonoBehaviour
     Ray ray;
     RaycastHit hit;
 
+    public bool validPlacement;
+
+    void Start()
+    {
+        // Start with no object selected
+        selected = null;
+    }
+
     // Update is called once per frame
     void Update()
     {
+        // Select and deselect and object
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out hit) && Input.GetMouseButtonDown(0))
         {
@@ -24,6 +33,12 @@ public class GameManager : MonoBehaviour
                 selected = null;
                 Debug.Log("no object");
             }
+        }
+
+        // Make selected object obvious
+        if (selected != null)
+        {
+            selected.transform.position = Input.mousePosition;
         }
     }
 }
--- DragAndDropRoom/Assets/MoveCamera.cs
+++ DragAndDropRoom/Assets/MoveCamera.cs
@@ -4,95 +4,32 @@ using UnityEngine;
 
 public class MoveCamera : MonoBehaviour
 {
-    public int UpDownMaxScroll;
-    public int LeftRightMaxScroll;
+    public float lookSpeedH = 2f;
+    public float lookSpeedV = 2f;
+    public float zoomSpeed = 5f;
+    public float dragSpeed = 12f;
 
-    public int MinZoom;
-    public int MaxZoom;
-    
-    public int ScrollSpeed;
-    public int ZoomSpeed;
+    private float yaw = 0f;
+    private float pitch = 0f;
 
-    public int MinPitch;
-    public int MaxPitch;
-
-    // Update is called once per frame
     void Update()
     {
-        /// KEYBOARD AND EDGE SCROLLING ///
-        /// 
-        // If mouse detected at the top of the screen
-        if ((Input.mousePosition.y >= Screen.height || Input.GetKey("up")) && Camera.main.gameObject.transform.position.z < UpDownMaxScroll)
+        //Look around with Right Mouse
+        if (Input.GetMouseButton(1))
         {
-            transform.Translate(Vector3.forward * Time.deltaTime * ScrollSpeed, Space.World);
+            yaw += lookSpeedH * Input.GetAxis("Mouse X");
+            pitch -= lookSpeedV * Input.GetAxis("Mouse Y");
+
+            transform.eulerAngles = new Vector3(pitch, yaw, 0f);
         }
 
-        // If mouse detected at the bottom of the screen
-        if ((Input.mousePosition.y <= 0 || Input.GetKey("down")) && Camera.main.gameObject.transform.position.z > -UpDownMaxScroll)
+        //drag camera around with Middle Mouse
+        if (Input.GetMouseButton(2))
         {
-            transform.Translate(Vector3.back * Time.deltaTime * ScrollSpeed, Space.World);
-        }
-        
-        // If mouse detected at the right of the screen
-        if ((Input.mousePosition.x >= Screen.width || Input.GetKey("right")) && Camera.main.gameObject.transform.position.x < LeftRightMaxScroll)
-        {
-            transform.Translate(Vector3.right * Time.deltaTime * ScrollSpeed, Space.World);
-        }
-        
-        // If mouse detected at the left of the screen
-        if ((Input.mousePosition.x <= 0 || Input.GetKey("left")) && Camera.main.gameObject.transform.position.x > -LeftRightMaxScroll)
-        {
-            transform.Translate(Vector3.left * Time.deltaTime * ScrollSpeed, Space.World);
+            transform.Translate(-Input.GetAxisRaw("Mouse X") * Time.deltaTime * dragSpeed, -Input.GetAxisRaw("Mouse Y") * Time.deltaTime * dragSpeed, 0);
         }
 
-        /// KEYBOARD AND MOUSE WHEEL SCROLLING ///
-        /// 
-        // Zoom in
-        if (((Input.GetAxis("Mouse ScrollWheel") > 0 || Input.GetKey("=")) && Camera.main.gameObject.transform.position.y > MinZoom) && !Input.GetMouseButton(2))
-        {
-            transform.Translate(Vector3.down * Time.deltaTime * ZoomSpeed, Space.World);
-        }
-
-        //Zoom out
-        if (((Input.GetAxis("Mouse ScrollWheel") < 0 || Input.GetKey("-")) && Camera.main.gameObject.transform.position.y < MaxZoom) && !Input.GetMouseButton(2))
-        {
-            transform.Translate(Vector3.up * Time.deltaTime * ZoomSpeed, Space.World);
-        }
-
-        /// KEYBOARD AND MOUSE BUTTON PITCH ///
-        /// 
-        // Pitch down
-        if ((Input.GetMouseButton(1) && Input.GetAxis("Mouse Y") < 0) || Input.GetKey("p"))
-        {
-            if (Camera.main.gameObject.transform.rotation.x <= 35)
-            {
-                Debug.Log("Min");
-                transform.Rotate(Vector3.right * Time.deltaTime * ZoomSpeed, Space.World);
-            }
-        }
-
-        // Pitch up
-        if ((Input.GetMouseButton(1) && Input.GetAxis("Mouse Y") > 0) || Input.GetKey("o"))
-        {
-            if (Camera.main.gameObject.transform.rotation.x <= 90)
-            {
-                Debug.Log("Max");
-                transform.Rotate(Vector3.left * Time.deltaTime * ZoomSpeed, Space.World);
-            }
-        }
-
-        /// KEYBOARD AND MOUSE BUTTON ROTATION ///
-        /// 
-        // Rotate right
-        if ((Input.GetMouseButton(2) && Input.GetAxis("Mouse X") > 0) || Input.GetKey("]"))
-        {
-            transform.Rotate(Vector3.down * Time.deltaTime * ZoomSpeed * 2, Space.World);
-        }
-
-        // Rotate left
-        if ((Input.GetMouseButton(2) && Input.GetAxis("Mouse X") < 0) || Input.GetKey("["))
-        {
-            transform.Rotate(Vector3.up * Time.deltaTime * ZoomSpeed * 2, Space.World);
-        }
+        //Zoom in and out with Mouse Wheel
+        transform.Translate(0, 0, Input.GetAxis("Mouse ScrollWheel") * zoomSpeed, Space.Self);
     }
 }
